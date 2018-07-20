ab905991b6d4d11cba79c38da0769eaccea6b954
Raveen Bouy <raveenshenal@gmail.com>
2018-07-21 02:59:59 +0530

    Finished login -  some parts

--- Project/MainWindow.xaml.cs
+++ Project/MainWindow.xaml.cs
@@ -20,6 +20,10 @@ namespace Project
     /// </summary>
     public partial class MainWindow : Window
     {
+        string userName = "";
+        string password = "";
+        int usernameLabelHover = 0;
+
         public MainWindow()
         {
             InitializeComponent();
@@ -53,6 +57,83 @@ namespace Project
         private void ForgotPasswordLeave(object sender, MouseEventArgs e)
         {
             ForgotPassword.TextDecorations = null;
+        }
+
+        //Username
+
+        private void UsernameLabelHover(object sender, MouseEventArgs e)
+        {
+            UsernameLabel.Visibility = Visibility.Hidden;
+            usernameLabelHover = 1;
+        }
+
+        private void UsernameTB_Text_Changed(object sender, TextChangedEventArgs e)
+        {
+            userName = UsernameTB.Text;
+        }
+
+        private void UsernameTB_GotFocus(object sender, RoutedEventArgs e)
+        {
+            UsernameLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void UsernameTB_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (UsernameTB.IsFocused == false && (userName == "" || userName == null))
+            {
+                UsernameLabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void UsernameTB_MouseEnter(object sender, MouseEventArgs e)
+        {
+            UsernameLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void UsernameTB_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (UsernameTB.IsFocused == false && (userName == "" || userName == null))
+            {
+                UsernameLabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        //Password
+
+        private void PasswordLabelHover(object sender, MouseEventArgs e)
+        {
+            PasswordLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void PasswordTB_GotFocus(object sender, RoutedEventArgs e)
+        {
+            PasswordLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void PasswordTB_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PasswordTB.IsFocused == false && (password == "" || password == null))
+            {
+                PasswordLabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void PasswordTB_MouseEnter(object sender, MouseEventArgs e)
+        {
+            PasswordLabel.Visibility = Visibility.Hidden;
+        }
+
+        private void PasswordTB_MouseLeave(object sender, MouseEventArgs e)
+        {
+            if (PasswordTB.IsFocused == false && (password == "" || password == null))
+            {
+                PasswordLabel.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void PasswordTB_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            password = PasswordTB.Text;
         }
     }
 }
